8a70df8e1522943fe0b6d618987f898cc28f97ad
Curtis Fowler <caf203@gmail.com>
2019-09-28 16:07:03 -0400

    converted lat lng addresses

--- src/routes/getallhouses.js
+++ src/routes/getallhouses.js
@@ -4,15 +4,6 @@ module.exports = (app) => {
     app.get('/getallhouses', async (req, res) => {
         console.log(req.headers)
         const documents = await getAllDocuments()
-        console.log(documents)
         res.status(200).send(documents)
     })
 }
-
-
-/*
-name
-address (street name)
-square footage
-
-*/
